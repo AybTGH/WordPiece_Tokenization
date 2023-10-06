16b4041cc331dfd9eb9d053a1559287b2f6432fe
atoudghi <ayoub.toudghi@auditeur.ec-lyon.fr>
2023-10-06 11:22:11 +0200

    add comments

--- WordPiece.py
+++ WordPiece.py
@@ -3,6 +3,15 @@ from collections import defaultdict
 
 
 class wordpiece: 
+    """
+    A class for WordPiece tokenization using the Hugging Face Transformers library.
+
+    Attributes:
+        corpus (str): The input text corpus to be tokenized.
+        vocab_size (int): The desired size of the token vocabulary.
+
+    Methods:
+    """
     
     def __init__(self, corpus, vocab_size=100) -> None:
         self.corpus = corpus
@@ -13,7 +22,12 @@ class wordpiece:
         self.vocab_final, self.splits_final= self.update_vocab(vocab_size)
     
     def pre_tokenize(self):
-        
+        """
+        Pre-tokenizes the input corpus using the BERT tokenizer and computes word frequencies.
+
+        Returns:
+            defaultdict: A dictionary containing word frequencies.
+        """
         tokenizer = AutoTokenizer.from_pretrained("bert-base-cased")
         word_freqs = defaultdict(int)
         words_with_offsets = tokenizer.backend_tokenizer.pre_tokenizer.pre_tokenize_str(self.corpus.strip())
@@ -24,6 +38,12 @@ class wordpiece:
         return word_freqs   
 
     def split_list(self):
+        """
+        Splits words in the corpus into subword pieces (subtokens) and stores them in a dictionary.
+
+        Returns:
+            dict: A dictionary mapping words to their subword splits.
+        """
         splits = {
         word: [c if i == 0 else f"##{c}" for i, c in enumerate(word)]
         for word in self.word_freqs.keys()
@@ -32,6 +52,12 @@ class wordpiece:
 
 
     def initial_vocab(self):
+        """
+        Creates the initial vocabulary including special tokens like [PAD], [UNK], [CLS], [SEP], and [MASK].
+
+        Returns:
+            list: A list containing the initial vocabulary tokens.
+        """
         alphabet = []
         for word in self.word_freqs.keys():
             if word[0] not in alphabet:
@@ -44,10 +70,16 @@ class wordpiece:
         
         return vocab
     
-
-    #function that computes the scores for each pair of successive elements in each word
     def compute_pair_scores(self, splits):
-        
+        """
+        Computes scores for each pair of successive subword elements in each word.
+
+        Args:
+            splits (dict): A dictionary mapping words to their subword splits.
+
+        Returns:
+            dict: A dictionary mapping subword pairs to their computed scores.
+        """
         letter_freqs = defaultdict(int)
         pair_freqs = defaultdict(int)
         for word, freq in self.word_freqs.items():
@@ -67,8 +99,18 @@ class wordpiece:
         }
         return scores
 
-#function that apply the apply the previous lerge in the splits dictionnary 
     def merge_pair(self, a, b, L):
+        """
+        Merges subword pieces 'a' and 'b' in the splits dictionary for all words in the corpus.
+
+        Args:
+            a (str): The first subword piece.
+            b (str): The second subword piece.
+            L (dict): A dictionary mapping words to their subword splits.
+
+        Returns:
+            dict: The updated dictionary of word splits after merging.
+        """
         for word in self.word_freqs:
             split = L[word]
             if len(split) == 1:
@@ -84,6 +126,15 @@ class wordpiece:
         return L
 
     def update_vocab(self, vocab_size):
+        """
+        Iteratively updates the vocabulary until it reaches the desired size using pair score computation and merging.
+
+        Args:
+            vocab_size (int): The desired size of the token vocabulary.
+
+        Returns:
+            tuple: A tuple containing the final vocabulary list and splits dictionary.
+        """
         splits_final = self.splits_initial
         vocab_final = self.vocab_initial
         while len(self.vocab_initial) < vocab_size:
@@ -103,6 +154,15 @@ class wordpiece:
         return vocab_final, splits_final
     
     def encode_word(self, word):
+        """
+        Encodes a word into a sequence of subtokens using the current vocabulary.
+
+        Args:
+            word (str): The input word to be encoded.
+
+        Returns:
+            list: A list of subtokens representing the encoded word.
+        """
         tokens = []
         while len(word) > 0:
             i = len(word)
@@ -117,6 +177,15 @@ class wordpiece:
         return tokens
     
     def tokenize(self, text):
+        """
+        Tokenizes a given input text into a sequence of subtokens using the WordPiece tokenization scheme.
+
+        Args:
+            text (str): The input text to be tokenized.
+
+        Returns:
+            list: A list of subtokens representing the tokenized text.
+        """
         tokenizer = AutoTokenizer.from_pretrained("bert-base-cased")
         pre_tokenize_result = tokenizer._tokenizer.pre_tokenizer.pre_tokenize_str(text)
         pre_tokenized_text = [word for word, offset in pre_tokenize_result]
